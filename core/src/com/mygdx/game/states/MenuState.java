6a38a657082ddec7ddb70f8fab814dd9451669ce
Jarl André Hübenthal <jarl.andre@gmail.com>
2017-04-14 20:33:31 +0200

    Part 4

---
+++ core/src/com/mygdx/game/states/MenuState.java
@@ -0,0 +1,34 @@
+package com.mygdx.game.states;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.FlappyBird;
+
+public class MenuState extends State {
+    private Texture background;
+    private Texture playBtn;
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playbtn.png");
+    }
+
+    @Override
+    void handleInput() {
+
+    }
+
+    @Override
+    void update(float dt) {
+
+    }
+
+    @Override
+    void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(background, 0, 0, FlappyBird.WIDTH, FlappyBird.HEIGHT);
+        sb.draw(playBtn, (FlappyBird.WIDTH / 2) - (playBtn.getWidth() / 2), (FlappyBird.HEIGHT / 2));
+        sb.end();
+    }
+}
